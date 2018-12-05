08706039d18213016e32436723d959deaeac4fa4
maxwellbenton <maxwellbenton@gmail.com>
2018-12-05 17:37:44 -0500

    drafts solution, tests

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-const testVar = {}
+class Song {
+  constructor( title, artist, genre ) {
+    this._title = title
+    this._artist = artist
+    this._genre = genre
+  }
 
-function testFunc() {
-  return "hi"
+  get title() {
+    return this._title
+  }
+
+  get artist() {
+    return this._artist
+  }
+
+  get caption() {
+    return `${this.title} by ${this.artist.name}`
+  }
+}
+
+class Artist {
+  constructor( name ) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set name( name ) {
+    this._name = name
+  }
+}
+
+class Genre {
+  constructor( name ) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
 }
--- test/indexTest.js
+++ test/indexTest.js
@@ -1,23 +1,77 @@
+describe( "index.js", () => {
 
 
-describe("index.html", () => {
-  it("includes 'hello'", () => {
-    expect(document.querySelector('body').innerHTML).to.include('hello')
-  })
 
-  it("includes div", () => {
-    expect(document.querySelector('body').innerHTML, "no empty div element found within body").to.include('<div></div>')
-  })
+  describe( "Genre", () => {
+    let rock
+    let pop
+    let soul
+    before( () => {
+      rock = new Genre( "Rock" )
+      pop = new Genre( "Pop" )
+      soul = new Genre( "Soul" )
+    } )
+
+    it( "has `name` property", () => {
+      expect( rock._name ).to.equal( "Rock" )
+      expect( pop._name ).to.equal( "Pop" )
+      expect( soul._name ).to.equal( "Soul" )
+    } )
+  } )
+  describe( "Artist", () => {
+    let artist
+    before( () => {
+      artist = new Artist( "Prince" )
+    } )
+
+    it( "has a private `_name` property", () => {
+      expect( artist._name ).to.equal( "Prince" )
+    } )
+
+    it( "has a `name` getter", () => {
+      expect( artist.name ).to.equal( "Prince" )
+    } )
+
+    it( "has a `name` setter", () => {
+      artist.name = "the artist formerly known as Prince"
+      expect( artist.name ).to.equal( "the artist formerly known as Prince" )
+      expect( artist._name ).to.equal( "the artist formerly known as Prince" )
+    } )
+  } )
 
 
-})
 
 
-describe("index.js", () => {
-  it("exists", () => {
-    expect(testVar).to.exist
-    expect(testVar).to.deep.equal({}) //without .deep, {} does not equal {}
-    expect(testFunc).to.exist
-    expect(testFunc()).to.equal('hi')
-  })
-})
+  describe( "Song", () => {
+    let rock
+    let artist
+    let purpleRain
+    before( () => {
+      rock = new Genre( "Rock" )
+      artist = new Artist( "Prince" )
+      purpleRain = new Song( "Purple Rain", artist, rock )
+    } )
+
+    it( "has a private `_title` property", () => {
+      expect( purpleRain._title ).to.equal( "Purple Rain" )
+    } )
+
+    it( "has a private `_artist` property that is assigned a Genre instance", () => {
+      expect( purpleRain._artist ).to.be.instanceof( Artist )
+      expect( purpleRain._artist ).to.equal( artist )
+      expect( purpleRain._artist._name ).to.equal( "Prince" )
+    } )
+
+    it( "has a private `_genre` property that is assigned a Genre instance", () => {
+      expect( purpleRain._genre ).to.be.instanceof( Genre )
+      expect( purpleRain._genre ).to.equal( rock )
+      expect( purpleRain._genre._name ).to.equal( "Rock" )
+    } )
+
+    it( "has a `caption` getter that returns a String with the title and artist name", () => {
+      expect( purpleRain.caption ).to.equal( "Purple Rain by Prince" )
+    } )
+
+  } )
+
+} )
