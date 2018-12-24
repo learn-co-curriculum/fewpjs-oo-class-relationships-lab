86cfb9b0843330c6e679efdd560c5cec3987c99c
maxwellbenton <maxwellbenton@gmail.com>
2018-12-24 09:35:55 -0500

    adds solution

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your classes here
+class Song {
+  constructor( title, artist, genre ) {
+    this._title = title
+    this._artist = artist
+    this._genre = genre
+  }
+
+  get title() {
+    return this._title
+  }
+
+  get artist() {
+    return this._artist
+  }
+
+  get genre() {
+    return this._genre
+  }
+
+  get caption() {
+    return `'${this.title}' by ${this.artist.name} (${this.genre.name})`
+  }
+}
+
+class Artist {
+  constructor( name ) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
+
+  set name( name ) {
+    this._name = name
+  }
+}
+
+class Genre {
+  constructor( name ) {
+    this._name = name
+  }
+
+  get name() {
+    return this._name
+  }
+}
